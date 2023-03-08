9c789739a59ed359bf1d9b3ee074808f784c49bf
Felipe <lipefelipe403@hotmail.com>
2023-03-08 16:15:35 -0300

    .

---
+++ pages/api/products/index.js
@@ -0,0 +1,64 @@
+import dbConnect from "@/db/mongo";
+import Product from "@/model/Menu";
+
+
+export default async function handler(req, res) {
+    const { method,
+        query: { id }
+    } = req;
+
+    dbConnect();
+    switch (method) {
+
+        case 'GET':
+            try {
+                const products = await Product.find();
+                res.status(200).json(products);
+            } catch (err) {
+                res.status(500).json(err);
+            }
+            break;
+
+        case 'POST':
+            try {
+                const product = await Product.create(req.body);
+                res.status(201).json(product)
+            } catch (err) {
+                res.status(500).json(err)
+            }
+            break;
+
+        case 'DELETE':
+            try {
+                console.log(id)
+                await Product.findByIdAndDelete(id);
+                res.status(200).json("O produto foi deletado");
+            } catch (err) {
+                res.status(500).json(err)
+            }
+
+            break;
+
+        case 'PUT':
+            try {
+
+                const { prodId } = req.query;
+                const formData = req.body;
+
+                if (prodId && formData) {
+                    await Product.findByIdAndUpdate(prodId, formData);
+                    res.status(200).json(formData)
+                }
+                res.status(404).json({ error: "Produto não encontrado" })
+            } catch (error) {
+                res.status(404).json(error)
+            }
+            break;
+
+
+    }
+
+
+
+
+}
---
+++ pages/index.js
@@ -0,0 +1,15 @@
+
+import Head from 'next/head';
+export default function Home() {
+  return (
+    <>
+      <Head>
+        <title></title>
+        <meta name="description" content="Melhor Comida da região" />
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+        <link rel="icon" href="https://cdn-icons-png.flaticon.com/512/12/12426.png" />
+      </Head>
+      <h1>index</h1>
+    </>
+  )
+}
